c9e89a8d2a438af0392b82dc8151181836c6fb18
Tristan Larue <tristanlarue@hotmail.com>
2026-02-11 12:40:09 -0500

    Basic Structure & Plan
    Look at Main.cpp for brainstorm

---
+++ src/Physics.cpp
@@ -0,0 +1,1 @@
+// This version will add radar signature, dynamic and more precice lift/drag calculations, terrain collision, wind, supersonic & hypersonic behaviors.
---
+++ src/Vizualisation.cpp
@@ -0,0 +1,1 @@
+//Use Magnum Engine with LLM made code to quickly vizualise terrain, entities, waves, trails, bounding box, etc.
